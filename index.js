2523abefb35681bcd2564b98b81c55e6b9f089b3
tamzidpeace <tamjedpeace@gmail.com>
2021-12-25 16:04:50 +0600

    simple changes

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const scan = () => {
     console.log('Hello');
 }
+
+
+function onPageFinishedFromFlutter(data) {
+    console.log(data);
+    return data;
+}
+
+function responseForFlutterRequest(data) {
+    console.log(data);
+    document.getElementById("result").innerHTML = data;
+    return;
+}
